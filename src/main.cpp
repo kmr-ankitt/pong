b64fa9ce631cf9a2fc8b963c307d36653691ec92
ankit <sahuankit.murhu@gmail.com>
2024-12-13 00:16:19 +0530

    2D object class

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,34 @@ const int WINDOW_HEIGHT = 720;
 
 int main()
 {
+/*
+*2D Vector class  
+*   - x, y are the coordinates of the vector
+*   - Here some convienent operators are overloaded so
+*     we can do something like `position += velocity * time` 
+*/
+class Vec2{
+    public:
+        float x, y;
+        
+        Vec2() : x(0.0f), y(0.0f) {}
+        Vec2(float x, float y): x(x), y(y) {}
+        
+        Vec2 operator+(Vec2 const& rhs){
+            return Vec2(x + rhs.x, y + rhs.y);
+        }
+        
+        Vec2& operator+=(Vec2 const& rhs){
+            x += rhs.x;
+            y += rhs.y;
+            return *this;
+        }      
+
+        Vec2 operator*(float rhs){
+            return Vec2(x * rhs, y * rhs);
+        }
+};
+
 	// Initialize SDL components
     SDL_Init(SDL_INIT_VIDEO);
 
