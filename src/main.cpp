1b09d5505534abdaa1530e6590486167f8896f3c
ankit <sahuankit.murhu@gmail.com>
2024-12-13 20:30:15 +0530

    Wall collision

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,8 @@ enum class CollisionType{
     Top,
     Middle,
     Bottom,
+    Left,
+    Right
 };
 
 // Stores the collision information
@@ -96,9 +98,30 @@ class Ball{
                 velocity.y = .75f * BALL_SPEED;
             }
         }
+        
+        /*
+        * CollideWithWall function is called when the ball collides with the wall
+        *  - If the ball collides with the top or bottom wall, the y velocity is reversed
+        *  - If the ball collides with the left or right wall, the ball is reset to the center
+        */
+        void CollideWithWall(Contact const& contact){
+            if((contact.type == CollisionType::Top) ||contact.type == CollisionType::Bottom){
+                position.y += contact.penetration;
+                velocity.y = -velocity.y;
+            }else if(contact.type == CollisionType::Left){
+                position.x = WINDOW_WIDTH / 2.0f;
+                position.y = WINDOW_HEIGHT / 2.0f;
+                velocity.x = BALL_SPEED;
+                velocity.y = 0.75f * BALL_SPEED;
+            }else if(contact.type == CollisionType::Right){
+                position.x = WINDOW_WIDTH / 2.0f;
+                position.y = WINDOW_HEIGHT / 2.0f;
+                velocity.x = -BALL_SPEED;
+                velocity.y = 0.75f * BALL_SPEED;
+            }
+        }
 };
-
-class Paddle{
+ class Paddle{
     public:
         Vec2 position;
         Vec2 velocity;
@@ -235,6 +258,28 @@ Contact CheckPaddleCollision(Ball const& ball, Paddle const& paddle){
         contact.type = CollisionType::Bottom;
     }
     
+    return contact;
+}
+
+Contact CheckWallCollision(Ball const& ball){
+    float ballLeft = ball.position.x;
+    float ballRight = ball.position.x + BALL_WIDTH;
+    float ballTop = ball.position.y;
+    float ballBottom = ball.position.y + BALL_HEIGHT;
+    
+    Contact contact{};
+    
+    if(ballLeft < 0.0f){
+        contact.type = CollisionType::Left;
+    }else if(ballRight > WINDOW_WIDTH){
+        contact.type = CollisionType::Right;
+    }else if(ballTop < 0.0f){
+        contact.type = CollisionType::Top;
+        contact.penetration = -ballTop;
+    }else if(ballBottom > WINDOW_HEIGHT){
+        contact.type = CollisionType::Bottom;
+        contact.penetration = WINDOW_HEIGHT - ballBottom;
+    }
     return contact;
 }
 
@@ -376,6 +421,8 @@ int main(){
 			    ball.CollideWithPaddle(contact);
 			}else if(contact = CheckPaddleCollision(ball, paddleRight); contact.type != CollisionType::None){
                 ball.CollideWithPaddle(contact);
+            }else if(contact = CheckWallCollision(ball); contact.type != CollisionType::None){
+                ball.CollideWithWall(contact);
             }
 			
 			// Clear the window to black
